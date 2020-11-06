9eed76768f068da8a85514a5b8744e27c5c00aaf
charlesdobson <charlesdobson92@gmail.com>
2020-11-05 22:32:23 -0500

    Small cleanup to bring up to date with Ergo Rank project

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,56 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ['react-hot-loader/patch', './src/index.js'],
-    mode: 'development',
-    target: 'web',
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules)/,
-                loader: 'babel-loader',
-                options: { 
-                    presets: [
-                        '@babel/env'
-                    ] 
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [ 
-                    'style-loader', 
-                    'css-loader'
-                ]
-            }
-        ]
-    },
-    resolve: { 
-        extensions: ['*', '.js', '.jsx'] 
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        port: 3000,
-        publicPath: 'http://localhost:3000/dist/',
-        hotOnly: true
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'public/index.html'
-        })
+  entry: ['react-hot-loader/patch', './src/index.js'],
+  devtool: '',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ['file-loader']
+      }
     ]
-}
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'src'),
+      'node_modules',
+      path.join(__dirname, 'public')
+    ],
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+      public: path.resolve(__dirname, 'public')
+    }
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './build',
+    port: 3000
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve('./public/index.html'),
+      favicon: './public/favicon.ico'
+    })
+  ]
+};
